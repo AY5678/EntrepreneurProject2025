bd5d6ee562686ba48c4e10101295b2c1e00161a2
AY5678 <sorryu404@gmail.com>
2025-05-11 13:38:53 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("whattf");
+        GachaSystem gs = new GachaSystem();
+        User user = new User("bob");
+        HomeworkSystem hs = new HomeworkSystem(user);
+
+        hs.completeHomework();
+
+        if (User.spendPoints())
+            GachaSystem.draw();
     }
 }
